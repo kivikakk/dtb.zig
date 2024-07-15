e5f50cb3dfba304802c899d5a5fec3ad7b66443c
Asherah Connor <ashe@kivikakk.ee>
2024-07-15 15:37:01 +0300

    build.zig: add standard optimize/target.

--- build.zig
+++ build.zig
@@ -1,8 +1,13 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
+
     _ = b.addModule("dtb", .{
         .root_source_file = b.path("src/dtb.zig"),
+        .optimize = optimize,
+        .target = target,
     });
 
     const test_step = b.step("test", "Run the tests");
